a6ccd1ade2160089d72ab7d6e17a207d11f89910
Joey Sisk <jjmsisk@gmail.com>
2021-01-18 20:40:55 -0600

    connected to jaws db in the connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "burgers_db",
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
